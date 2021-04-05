aada120d19e08776d4585c89caa1bda76befab33
Alexander <Sakeran@gmail.com>
2021-04-05 13:46:19 -0400

    Style recipe summary sections

---
+++ src/main.js
@@ -0,0 +1,25 @@
+// Add .lazybg class to any recipe summaries.
+// Ensures the background image, if any, is lazy-loaded.
+(() => {
+  const observer = new IntersectionObserver(
+    (entries, observer) => {
+      for (let e of entries) {
+        if (e.isIntersecting) {
+          const target = e.target;
+
+          target.classList.remove("lazybg");
+          observer.unobserve(target);
+        }
+      }
+    },
+    {
+      rootMargin: "20%",
+    }
+  );
+
+  const summaries = document.querySelectorAll(".recipe-summary");
+  summaries.forEach((s) => {
+    s.classList.add("lazybg");
+    observer.observe(s);
+  });
+})();
